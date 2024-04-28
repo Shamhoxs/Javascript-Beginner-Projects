1cd93bcba2e79cf579e9d0bc69c919df39da41cf
Shamhoxs <shamilasiva03@gmail.com>
2024-04-28 21:34:02 +0530

    Questions +styles added

---
+++ Quiz App/script.js
@@ -0,0 +1,39 @@
+const questios=[
+    {
+        question:"Full form of HTML?",
+        answers:[
+            {text:"Hypertext making language",correct:false},
+            {text:"Hypertext major language",correct:false},
+            {text:"Hypertext markup language",correct:true},
+            {text:"Hyperturn markup language",correct:false},
+        ]
+    },
+    {
+        question:"Full form of CSS?",
+        answers:[
+            {text:"Cascading styling sheets",correct:true},
+            {text:"Casting styling sheets",correct:false},
+            {text:"Cascading size sheets",correct:false},
+            {text:"Casting size sheets",correct:false},
+        ]
+    },
+    {
+        question:"How to comment in HTML?",
+        answers:[
+            {text:"//",correct:false},
+            {text:"<*/>>",correct:false},
+            {text:"/<>/",correct:false},
+            {text:"<!-- -->>",correct:true},
+            
+        ]
+    },
+    {
+        question:"How to print in console?",
+        answers:[
+            {text:"console.log(a)",correct:true},
+            {text:"console.login(a)",correct:false},
+            {text:"consolelog(a)",correct:false},
+            {text:"Hconsole.log.(a)",correct:false},
+        ]
+    },
+]
